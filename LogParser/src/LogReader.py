7fe2e59bf540ac49c9c9894ae9dcc9c94ebd65ac
Vaisah-RS <vaisakhrs4@gmail.com>
2026-09-01 21:06:39 +0530

    Add LogReader to read and parse logs from different sources

--- LogParser/src/LogReader.py
+++ LogParser/src/LogReader.py
@@ -1,4 +1,5 @@
 import yaml
+from LogParser import LogParser
 
 #Load and read the config file
 def load_config(configPath):
@@ -7,5 +8,20 @@ def load_config(configPath):
         return logPath
 
 class LogReader:
-    def __init__(self):
-        Load_Config("config.yaml")
+    def __init__(self,config_path):
+        self.config_data=load_config(config_path)
+
+    def parse_all(self):
+       result={}
+       for source in self.config_data["sources"]:
+           name=source["name"]
+           path=source["path"]
+           parser=LogParser(path)
+           parser.parse()
+           result[name]=parser.log_entries
+       return result
+
+
+test=LogReader("LogParser\config.yaml")
+res=test.parse_all()
+print(res)
