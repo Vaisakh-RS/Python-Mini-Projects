071c18556e701889b955d8e1ee7caa6ab4894eac
Vaisah-RS <vaisakhrs4@gmail.com>
2026-09-03 20:07:22 +0530

    Add pairing of logs based on timestamps

--- LogParser/src/LogReader.py
+++ LogParser/src/LogReader.py
@@ -1,5 +1,9 @@
 import yaml
+from datetime import datetime
 from LogParser import LogParser
+import logging
+
+logger=logging.getLogger(__name__)
 
 #Load and read the config file
 def load_config(configPath):
@@ -21,21 +25,28 @@ class LogReader:
            self.result[name]=parser.log_entries
        return self.result
 
+    #filter by log level from auth and db logs
     def filter(self):
-      # Safely combines the two lists first, then filters the levels
-        combined_lists = self.result.get('auth', []) + self.result.get('database', [])
+        auth_logs = [entry for entry in self.result.get('auth',[]) if entry.log_level in ('ERROR', 'WARNING')]
+        db_logs = [entry for entry in self.result.get('database',[]) if entry.log_level in ('ERROR', 'WARNING')]
+        return auth_logs,db_logs
 
-        filtered_logs = [
-            entry for entry in combined_lists 
-            if entry.log_level in ('ERROR', 'WARNING')
-        ]
-
-        return filtered_logs
-
-
+    def pair_logs(self,auth_errors,db_errors,window_seconds=150):
+        pairs=[]
+        for x in auth_errors:
+            x_time=datetime.strptime(x.timestamp,"%Y-%m-%d %H:%M:%S")
+            for y in db_errors:
+                 y_time=datetime.strptime(y.timestamp,"%Y-%m-%d %H:%M:%S")
+                 time_diff=abs(x_time-y_time)
+                 if time_diff.total_seconds()<=window_seconds:
+                     pairs.append((x,y))
+            logger.info(f"Found {len(pairs)} correlated pairs")
+        return pairs
 
 test=LogReader("LogParser\config.yaml")
 res=test.parse_all()
-res2=test.filter()
-print(res2)
+auth_errors,db_errors=test.filter()
+pairs=test.pair_logs(auth_errors,db_errors)
+print(pairs)
+
 
